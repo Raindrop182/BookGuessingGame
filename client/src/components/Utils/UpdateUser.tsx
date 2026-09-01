22ce866d9ea2e45b6c914de3026049d9c0c5d297
Raine <rainew182@gmail.com>
2026-08-31 18:25:26 -0700

    Refactor: add helper function in UpdateUser

--- client/src/components/Utils/UpdateUser.tsx
+++ client/src/components/Utils/UpdateUser.tsx
@@ -2,42 +2,44 @@ import { useUser } from "./UserContext";
 import { API_URL } from "./api";
 
 export const useUpdateUser = () => {
-  const { setUser } = useUser();
-  const addBookGuess = async (bookId: number, numQuotes: number) => {
-    await fetch(`${API_URL}/api/user`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ booksGuessed: [{ bookId, numQuotes }] }),
-      credentials: "include",
-    })
-      .then((res) => res.json())
-      .then(setUser);
+  const {user, setUser } = useUser();
+
+  const sendUpdate = async (payload: Record<string, any>) => {
+    if (!user) return;
+    try {
+      const res = await fetch(`${API_URL}/api/user`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+        credentials: "include",
+      });
+
+      if (!res.ok) throw new Error("Update failed");
+      const updatedUser = await res.json();
+      setUser(updatedUser);
+    } catch (err) {
+      console.error(err);
+    }
   };
+
+  const addBookGuess = async (bookId: number, numQuotes: number) => {
+    sendUpdate({ booksGuessed: [{ bookId, numQuotes }] })
+  };
+
   const addBODStat = async (status: string, numQuotes: number) => {
-    await fetch(`${API_URL}/api/user`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
+    sendUpdate({
         bookofthedayStats: {
           status: status,
           numQuotes: numQuotes,
           date: new Date().toDateString(),
         },
-      }),
-      credentials: "include",
-    })
-      .then((res) => res.json())
-      .then(setUser);
+      })
   };
-  const changeAvatarColor = (color: string) => {
-    setUser((prev) => (prev ? { ...prev, avatarColor: color } : null));
 
-    fetch(`${API_URL}/api/user`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ avatarColor: color }),
-      credentials: "include",
-    }).catch(() => console.error("Failed to update avatar color"));
+  const changeAvatarColor = (color: string) => {
+    console.log("Updating new color ${color}")
+    sendUpdate({ avatarColor: color })
   };
+
   return { addBookGuess, addBODStat, changeAvatarColor };
 };
